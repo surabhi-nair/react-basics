d35e68311793be2dec8500907ab235ede1241e7a
Surabhi Nair <surabhi@litmus7.com>
2021-07-27 20:46:30 +0530

    added multi-page projects

---
+++ multi-page-complex-project/src/components/layout/MainNavigation.js
@@ -0,0 +1,20 @@
+import classes from './MainNavigation.module.css';
+import { NavLink } from 'react-router-dom';
+
+const MainNavigation = () => {
+    return <header className={ classes.header }>
+        <div className={ classes.logo }>Great quotes</div>
+        <nav className={ classes.nav }>
+            <ul>
+                <li>
+                    <NavLink to='/quotes' activeClassName={ classes.active }>All quotes</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/new-quote' activeClassName={ classes.active }>Add quotes</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </header>;
+};
+
+export default MainNavigation;
---
+++ multi-page-complex-project/src/pages/NewQuote.js
@@ -0,0 +1,24 @@
+import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
+import { useEffect } from 'react';
+
+const NewQuote = () => {
+    const { sendRequest, status } = useHttp( addQuote );
+    const history = useHistory();
+
+    useEffect( () => {
+        if ( status === 'completed' ) {
+            history.push( '/quotes' );
+        }
+    }, [ status, history ] );
+
+    const onAddQuoteHandler = quoteData => {
+        sendRequest( quoteData );
+    };
+
+    return <QuoteForm isLoading={ status === 'pending' } onAddQuote={ onAddQuoteHandler } />;
+};
+
+export default NewQuote;
